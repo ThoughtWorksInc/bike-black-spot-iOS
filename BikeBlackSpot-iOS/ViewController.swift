28db5cda4e0339f3d21386b955eb2df49b11efa3
Sarah Nelson <Sarah@Sarahs-MacBook-Pro.local>
2015-07-01 12:12:57 +1000

    Initial project setup

---
+++ BikeBlackSpot-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BikeBlackSpot-iOS
+//
+//  Created by Sarah Nelson on 1/07/2015.
+//  Copyright (c) 2015 ThoughtWorks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
