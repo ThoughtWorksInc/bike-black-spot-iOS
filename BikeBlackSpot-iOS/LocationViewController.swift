52cf3ca28130ae77f33ffa3e94a84c86057fd8c2
Liam Ketchup <lketchup@thoughtworks.com>
2015-07-16 16:11:14 +1000

    Location View can now drag a marker with a long press.

--- BikeBlackSpot-iOS/LocationViewController.swift
+++ BikeBlackSpot-iOS/LocationViewController.swift
@@ -1,40 +1,26 @@
 import UIKit
 import GoogleMaps
 
-class LoctionViewController: UIViewController {
-
+class LocationViewController: UIViewController, GMSMapViewDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        var target: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 51.6, longitude: 17.2)
-        var camera: GMSCameraPosition = GMSCameraPosition(target: target, zoom: 6, bearing: 0, viewingAngle: 0)
+        var camera = GMSCameraPosition.cameraWithLatitude(-37.816684, longitude: 144.963962, zoom: 15)
+        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        mapView.delegate = self
+        self.view = mapView
         
-        var gmaps = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
-        gmaps.myLocationEnabled = true
-        gmaps.camera = camera
-        
-        self.view = gmaps
-        
-        //        self.view = mapView
-        
-        //        var camera = GMSCameraPosition.cameraWithLatitude(-33.868,
-//            longitude:151.2086, zoom:6)
-//        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera:camera)
-//        
-//        var marker = GMSMarker()
-//        marker.position = camera.target
-//        marker.snippet = "Hello World"
-//        marker.appearAnimation = kGMSMarkerAnimationPop
-//        marker.map = mapView
-//        
-//        self.view = mapView
+        var marker = GMSMarker()
+        marker.position = CLLocationCoordinate2DMake(-37.816684, 144.963962)
+        marker.draggable = true
+        marker.map = mapView
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
