b6b5c2d8f72927c32d8cced44ef0266394dd2040
John Geddes <jgeddes@thoughtworks.com>
2015-07-23 14:37:56 +1000

    #103 Added basic button to photo page. Testing: Camera opens when button is pressed

--- BikeBlackSpot-iOS/PhotoViewController.swift
+++ BikeBlackSpot-iOS/PhotoViewController.swift
@@ -1,17 +1,46 @@
 import UIKit
+import Cartography
 
 class PhotoViewController: UIViewController {
+    
+    var takePhotoButton:UIButton?
+    var galleryPhotoButton:UIButton?
+    let picker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        takePhotoButton = UIButton()
+        takePhotoButton!.setTitle("Take A Photo", forState: UIControlState.Normal)
+        takePhotoButton!.backgroundColor = UIColor.redColor()
         
+        takePhotoButton!.addTarget(self, action: "openCamera:", forControlEvents: UIControlEvents.TouchUpInside)
+        
+        self.view.addSubview(takePhotoButton!)
+        
+        constrain(takePhotoButton!) { takePhotoButton in
+            takePhotoButton.center == takePhotoButton.superview!.center
+            takePhotoButton.width == takePhotoButton.superview!.width * 0.8
+            takePhotoButton.height == 50
+        }
+        
+        galleryPhotoButton = UIButton()
+        galleryPhotoButton!.setTitle("Select A Photo From Gallery", forState: UIControlState.Normal)
         
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func openCamera(sender:UIButton!)
+    {
+        println("Button tapped")
+        picker.allowsEditing = false
+        picker.sourceType = UIImagePickerControllerSourceType.Camera
+        picker.cameraCaptureMode = .Photo
+        presentViewController(picker, animated: true, completion: nil)
     }
     
     
