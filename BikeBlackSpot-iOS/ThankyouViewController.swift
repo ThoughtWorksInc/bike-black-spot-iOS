7678d248fc16bcd99e5a363c3e3c6e24a3378662
Anita Santoso <santoso.anita@gmail.com>
2015-07-27 16:20:38 +1000

    Enable new report button on thank you screen

--- BikeBlackSpot-iOS/ThankyouViewController.swift
+++ BikeBlackSpot-iOS/ThankyouViewController.swift
@@ -17,34 +17,43 @@ class ThankyouViewController: UIViewController {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        SwiftLoader.show(animated: true)
         
-        self.view.enableUserInteraction(false) // why doesn't this work?
-        doneButton.enabled = false
-        
+        setBusy(true)
         // TODO should this be done in previous screen?
         
         var isRegistered = UserTokenMgr.sharedInstance.hasToken()
         let report = Report.getCurrentReport()
         if(!isRegistered) {
+
             if let user = report.user {
                 APIService.sharedInstance.registerUser(user)
                     .then { uuid -> Void in
-                        SwiftLoader.hide()
-                        
-                        self.view.enableUserInteraction(true)
-                        self.doneButton.enabled = true
-                        
+                        self.setBusy(false)
                         UIAlertView(title: "Success", message: "User has been registered", delegate: nil, cancelButtonTitle: "OK").show()
                         
                         // TODO call create report here
                     }
                     .catch { error in
+                        self.setBusy(false)
                         UIAlertView(title: "Error", message: "Error registering user", delegate: nil, cancelButtonTitle: "OK").show()
                     }
             }
         } else {
+            setBusy(false)
             SwiftLoader.hide()
+        }
+    }
+    
+    // TODO move to FormViewController
+    func setBusy(busy:Bool) {
+        if(busy) {
+            SwiftLoader.show(animated: true)
+            self.view.enableUserInteraction(false) // why doesn't this work?
+            doneButton.enabled = false
+        } else {
+            SwiftLoader.hide()
+            self.view.enableUserInteraction(true)
+            self.doneButton.enabled = true
         }
     }
     
