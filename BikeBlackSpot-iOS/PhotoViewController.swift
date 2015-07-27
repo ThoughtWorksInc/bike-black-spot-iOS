9a5f4e226bb6511ece4f4c5b31b2f22a0a208e76
John Geddes <jgeddes@thoughtworks.com>
2015-07-27 15:46:11 +1000

    #103 Merging xproj

--- BikeBlackSpot-iOS/PhotoViewController.swift
+++ BikeBlackSpot-iOS/PhotoViewController.swift
@@ -15,38 +15,42 @@ class PhotoViewController: UIViewController,UIImagePickerControllerDelegate,UINa
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        UILabel.appearance().substituteFontName = "AlternateGothicLT-No2"
+        
         buttonSeparatorLabel = UILabel()
-        
-        //let cameraIcon = UIImage(named: "camera_button.png") as UIImage!
-        //var galleryIcon = UIImage()
-        
-        //println(cameraIcon!.size.width)
         
         picker.delegate = self
         
         buttonSeparatorLabel!.text = "OR"
         
+        
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        UIImage(named: "background.png")?.drawInRect(self.view.bounds)
+        
+        var backgroundImage: UIImage = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+        
+        self.view.backgroundColor = UIColor(patternImage: backgroundImage)
+        
         takePhotoButton.setTitle("take a photo".uppercaseString, forState: UIControlState.Normal)
         takePhotoButton.setTitleColor(UIColor.greenColor(), forState: .Highlighted)
         takePhotoButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
-        takePhotoButton.titleLabel!.font = UIFont(name: "AlternateGothicLT-No2", size: 16)
-        //takePhotoButton.titleEdgeInsets = UIEdgeInsetsMake(0, -300, 0, 20) //400
-        //takePhotoButton.backgroundColor = UIColor.redColor()
-        //takePhotoButton.setImage(cameraIcon, forState: UIControlState.Normal)
+        //takePhotoButton.titleLabel!.font = UIFont(name: "AlternateGothicLT-No2", size: 16)
         takePhotoButton.setBackgroundImage(UIImage(named: "camera_button.png"), forState: UIControlState.Normal)
-        //takePhotoButton.imageEdgeInsets = UIEdgeInsetsMake(5, 5, 5, 200) //200
         takePhotoButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
-        //takePhotoButton.contentHorizontalAlignment = UIControlContentHorizontalAlignment.Right
-        takePhotoButton.layer.borderWidth = 2
-        takePhotoButton.layer.borderColor = UIColor.orangeColor().CGColor
-        takePhotoButton.layer.cornerRadius = 5
-        //takePhotoButton!.contentEdgeInsets = UIEdgeInsetsMake(0, 200, 0, 200)
         
         takePhotoButton.addTarget(self, action: "openCamera:", forControlEvents: UIControlEvents.TouchUpInside)
         
         
-        galleryPhotoButton.setTitle("Select A Photo From Gallery", forState: .Normal)
-        galleryPhotoButton.addTarget(self, action: "openCamera:", forControlEvents: UIControlEvents.TouchUpInside)
+        galleryPhotoButton.setTitle("Select A Photo From Gallery".uppercaseString, forState: .Normal)
+        galleryPhotoButton.setTitleColor(UIColor.greenColor(), forState: .Highlighted)
+        galleryPhotoButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        galleryPhotoButton.titleLabel!.font = UIFont(name: "AlternateGothicLT-No2", size: 16)
+        galleryPhotoButton.setBackgroundImage(UIImage(named: "album_button.png"), forState: UIControlState.Normal)
+        galleryPhotoButton.imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+        
+        galleryPhotoButton.addTarget(self, action: "openPhotoGallery:", forControlEvents: UIControlEvents.TouchUpInside)
         
         self.view.addSubview(buttonSeparatorLabel!)
         self.view.addSubview(takePhotoButton)
@@ -54,23 +58,20 @@ class PhotoViewController: UIViewController,UIImagePickerControllerDelegate,UINa
         
         constrain(takePhotoButton) { takePhotoButton in
             takePhotoButton.centerX == takePhotoButton.superview!.centerX
-            takePhotoButton.centerY == takePhotoButton.superview!.centerY - 75
+            takePhotoButton.centerY == takePhotoButton.superview!.centerY - 50
             takePhotoButton.width == takePhotoButton.superview!.width * 0.8
             takePhotoButton.height == takePhotoButton.width * self.buttonAspectRatio
         }
         
         constrain(buttonSeparatorLabel!) { buttonSeparatorLabel in
             buttonSeparatorLabel.center == buttonSeparatorLabel.superview!.center
-            buttonSeparatorLabel.width == buttonSeparatorLabel.superview!.width * 0.8
-            buttonSeparatorLabel.height == 20
         }
         
         constrain(galleryPhotoButton) { galleryPhotoButton in
-            //galleryPhotoButton.center == galleryPhotoButton.superview!.center
             galleryPhotoButton.centerX == galleryPhotoButton.superview!.centerX
-            galleryPhotoButton.centerY == galleryPhotoButton.superview!.centerY + 75
+            galleryPhotoButton.centerY == galleryPhotoButton.superview!.centerY + 50
             galleryPhotoButton.width == galleryPhotoButton.superview!.width * 0.8
-            galleryPhotoButton.height == galleryPhotoButton.superview!.height * 0.3334
+            galleryPhotoButton.height == galleryPhotoButton.width * self.buttonAspectRatio
         }
         
     }
@@ -83,7 +84,6 @@ class PhotoViewController: UIViewController,UIImagePickerControllerDelegate,UINa
     // http://makeapppie.com/2014/12/04/swift-swift-using-the-uiimagepickercontroller-for-a-camera-and-photo-library/
     func openCamera(sender:UIButton!)
     {
-        println(sender.frame.size.width)
         if UIImagePickerController.availableCaptureModesForCameraDevice(.Rear) != nil {
             picker.allowsEditing = false
             picker.sourceType = UIImagePickerControllerSourceType.Camera
@@ -100,13 +100,22 @@ class PhotoViewController: UIViewController,UIImagePickerControllerDelegate,UINa
         }
     }
     
+    func openPhotoGallery(sender:UIButton!)
+    {
+        picker.allowsEditing = false
+        picker.sourceType = .PhotoLibrary
+        presentViewController(picker, animated: true, completion: nil)
+    }
+    
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [NSObject : AnyObject]) {
         var chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
         Report.getCurrentReport().image = UIImageJPEGRepresentation(chosenImage, 1.0) //http://pinkstone.co.uk/how-to-save-a-uiimage-in-core-data-and-retrieve-it/
         println("Image taken~!")
         dismissViewControllerAnimated(true, completion: nil)
+        self.navigationController!.pushViewController(ReviewViewController(), animated: true)
         
     }
+    
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
     }
