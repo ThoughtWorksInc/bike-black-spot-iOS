b8c78d48a921ce46b560ab355c5bcd8e08e3b9cc
John Geddes <jgeddes@thoughtworks.com>
2015-07-27 16:30:42 +1000

    #103 Merges! Merges EVERYWHERE! Also removed broken segue from buttons in photoview

--- BikeBlackSpot-iOS/PhotoViewController.swift
+++ BikeBlackSpot-iOS/PhotoViewController.swift
@@ -112,7 +112,7 @@ class PhotoViewController: UIViewController,UIImagePickerControllerDelegate,UINa
         Report.getCurrentReport().image = UIImageJPEGRepresentation(chosenImage, 1.0) //http://pinkstone.co.uk/how-to-save-a-uiimage-in-core-data-and-retrieve-it/
         println("Image taken~!")
         dismissViewControllerAnimated(true, completion: nil)
-        self.navigationController!.pushViewController(ReviewViewController(), animated: true)
+        //self.navigationController!.pushViewController(ReviewViewController(), animated: true)
         
     }
     
