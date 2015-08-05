315f5350bc42302c9ffe73b888c0b578eb63374d
Claire Dodd <cdodd@thoughtworks.com>
2015-08-05 11:27:14 +1000

    If categories is not loaded, on app becoming active reattempt to load them

--- BikeBlackSpot-iOS/AppDelegate.swift
+++ BikeBlackSpot-iOS/AppDelegate.swift
@@ -76,6 +76,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        if Categories.isNotLoaded() {
+            Categories.resetNumberOfAttempts()
+            Categories.loadCategories()
+        }
     }
     
     func applicationWillTerminate(application: UIApplication) {
